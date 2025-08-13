6d984a52cbc016d6f9836464c5d0cc2c1a29ffbc
Wang Gang <wang.vai@gmail.com>
2025-08-13 15:25:37 +0800

    Revise to use view model

---
+++ client/src/views/FileUploaderView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FileUploader from '../components/FileUploader.vue';
+</script>
+
+<template>
+    <div>
+        <FileUploader />
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ client/src/views/PlayMusicView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import playMusic from '../components/playMusic.vue';
+</script>
+
+<template>
+    <div>
+        <playMusic />
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
